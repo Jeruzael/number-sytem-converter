ac787afb739091a2d28ee634838e2247b82c746b
level one <dumale.jeruzael.bscs2019@gmail.com>
2021-11-14 20:58:08 +0800

    messaging function

---
+++ js/chat.js
@@ -0,0 +1,18 @@
+let messages = [];
+let messCont = document.querySelector('#messageCont');
+let container = document.getElementById("container");
+let message = document.querySelector('#chatBox');
+let sendBtn = document.querySelector('#sendBtn');
+
+
+
+function createMess(){
+	let newMess = document.createElement('div');
+	newMess.className = "newMessage";
+	newMess.innerHTML = message.value;
+	messCont.appendChild(newMess);
+	//console.log("test: ", messCont);
+}
+
+
+sendBtn.addEventListener('click', createMess);
