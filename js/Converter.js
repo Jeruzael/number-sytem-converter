2854d8a5432c128f638ecdecf320e3a4d12b53b9
level one <dumale.jeruzael.bscs2019@gmail.com>
2021-11-18 19:29:48 +0800

    functions added

--- js/Converter.js
+++ js/Converter.js
@@ -1,26 +1,36 @@
 
 
 let DecimalTo = class {
-	constructor(divisor){
-		this.divisor = divisor;
-		
+	constructor(divisor, dividend){
+		this.divisor = divisor;	
+		this.dividend = dividend;	
 	}
 
-	getQuotient(dividend) {
-		let quo = dividend/this.divisor;		
+	getQuotient() {
+		let quo = this.dividend/this.divisor;
 		//return Number.parseFloat(quo).toFixed(2);
+		//console.log(dividend%this.divisor);
 		return Math.trunc(quo);
 		//return Number.parseInt(Number.parseFloat(quo).toFixed(0));
 		//return quo;
 	}
 
+	getModulo(){
+		let mod = this.dividend%this.divisor;
+		return mod;
+	}
+
+	getSubtrahend(){
+		let sbtd = this.divisor * this.getQuotient();
+		return sbtd;
+	}
 
 };
 
-let decToBin = new DecimalTo(2);
-let decToOctal = new DecimalTo(8);
-let decToHexa = new DecimalTo(16);
+let decToBin = new DecimalTo(2, 100);
+let decToOctal = new DecimalTo(8, 100);
+let decToHexa = new DecimalTo(16, 100);
 
-console.log("Bin: ", decToBin.getQuotient(100));
-console.log("Octal: ", decToOctal.getQuotient(100));
-console.log("Hexa: ", decToHexa.getQuotient(100));
+console.log("Bin: ", decToBin.getQuotient(), " Modulo: ", decToBin.getModulo(), " Subtrahend: ", decToBin.getSubtrahend());
+console.log("Octal: ", decToOctal.getQuotient(), " Modulo: ", decToOctal.getModulo(), " Subtrahend: ", decToOctal.getSubtrahend());
+console.log("Hexa: ", decToHexa.getQuotient(), " Modulo: ", decToHexa.getModulo(), " Subtrahend: ", decToHexa.getSubtrahend());
